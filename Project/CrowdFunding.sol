b0fda9745d527bf541f8f3c4d20bec464b829f1c
NilsonKr <nuotrok444@gmail.com>
2021-11-13 16:38:09 -0500

    :sparkles: CrowdFunding contract improvements
    List to storage different projects and a history of contributions to each contract

--- Project/CrowdFunding.sol
+++ Project/CrowdFunding.sol
@@ -2,46 +2,84 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-
 contract CrowdFundingSmartContract {
-    address payable projectOwner;
-    string public projectName = "Dummie Project";
-    string public projectState = "Closed";
-    uint public funds;
-    uint public fundRaisingGoal;
-    
-    constructor(string memory _name, string memory state, uint fundsGoal){
-        projectOwner = payable(msg.sender);
-        projectState = state; 
-        projectName = _name;
-        fundRaisingGoal = fundsGoal;
+    struct Project {
+        address payable projectOwner;
+        string  projectName;
+        string  projectState;
+        uint  funds;
+        uint  fundRaisingGoal;
     }
 
-    event ProjectFunded(address from, uint funds);
+    struct Contribution {
+        address contributor;
+        uint ammount;
+        uint timestamp;
+    }
+
+    Project[] public projectList;
+
+    mapping (string => Contribution[]) public fundsHistory;
+
+    
+    function createProject(string memory _projectName, uint _fundRaisingGoal) public payable returns(Project memory){
+        Project memory project = Project({ 
+            projectOwner: payable(msg.sender), 
+            projectName: _projectName, 
+            projectState: "Funding", 
+            funds: 0, 
+            fundRaisingGoal: _fundRaisingGoal 
+        });
+        projectList.push(project);
+        return project;
+    }
+
+    event ProjectFunded(string projectName, Contribution contribution);
     event ProjectStateChanged(string newState, string oldState);
 
-    modifier onlyOwner {
-        require(msg.sender == projectOwner, "You need to be the project's owner");
+    modifier onlyOwner(uint index) {
+        require(msg.sender == projectList[index].projectOwner, "You need to be the project's owner");
         _;
     }
 
-    modifier contributers {
-        require(payable(msg.sender) != projectOwner, "You cannot contribute to your own project");
+    modifier contributers(uint index) {
+        require(payable(msg.sender) != projectList[index].projectOwner, "You cannot contribute to your own project");
         _;
     }
 
-    function fundProject() public payable contributers {
-        require(keccak256(abi.encodePacked(projectState)) != keccak256(abi.encodePacked("Closed")), "The project is closed");
+    function fundProject(uint projIndex) public payable contributers(projIndex) {
+        Project memory targetProject = projectList[projIndex];
+        require(keccak256(abi.encodePacked(targetProject.projectState)) != keccak256(abi.encodePacked("Closed")), "The project is closed");
         require(msg.value > 0, "Cannot contribute with 0");
-        projectOwner.transfer(msg.value);
-        funds += msg.value;
-        emit ProjectFunded(msg.sender, msg.value);
+
+        Contribution memory newContribution = Contribution({
+            contributor: msg.sender,
+            ammount: msg.value,
+            timestamp: block.timestamp
+        }); 
+
+        targetProject.projectOwner.transfer(msg.value);
+        targetProject.funds += msg.value;
+        
+        projectList[projIndex] = targetProject;
+        fundsHistory[targetProject.projectName].push(newContribution);
+
+        emit ProjectFunded(targetProject.projectName, newContribution);
     }
 
-    function changeProjectState (string memory newState) public onlyOwner {
-        require(keccak256(abi.encodePacked(projectState)) != keccak256(abi.encodePacked(newState)), "This is already the project state");
-        string memory oldProjectState = projectState;
-        projectState = newState;
-        emit ProjectStateChanged(projectState, oldProjectState);
+    function changeProjectState (uint projIndex, string calldata newState) public onlyOwner(projIndex) {
+        Project memory targetProject = projectList[projIndex];
+        require(keccak256(abi.encodePacked(targetProject.projectState)) != keccak256(abi.encodePacked(newState)), "This is already the project state");
+        
+        string memory oldState = targetProject.projectState;
+        targetProject.projectState = newState;
+
+        projectList[projIndex] = targetProject;
+        emit ProjectStateChanged(targetProject.projectState, oldState);
+    }
+
+
+    function getContributions(string memory projectName) public view returns(Contribution[] memory){
+        return fundsHistory[projectName];
     }
 }
